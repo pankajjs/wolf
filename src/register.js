cfb05a9105d6cc75066ff33002c18ee5585554d6
Pankaj <pankajshadev@gmail.com>
2025-07-01 21:23:35 +0530

    feat: setup discord bot interaction
    feat: add handler to register commands
    
    feat: add hello command

---
+++ src/register.js
@@ -0,0 +1,32 @@
+import dotenv from "dotenv";
+import { HELLO } from "./commands";
+
+dotenv.config();
+
+const commands = [
+   HELLO
+];
+
+async function registerCommands(appId, discordToken, commands) {
+    const endpoint = `https://discord.com/api/v10/applications/${appId}/commands`
+    try{
+        const res = await fetch(endpoint, {
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Bot ${discordToken}`,
+            },
+            method: 'PUT',
+            body: JSON.stringify(commands),
+          });
+        if(!res.ok){
+            console.error("Failed to register command");
+            return;
+        }
+        console.log("Commands registered successfully");
+    }catch(error){
+        console.error(`Error while registering commands`, error);
+    }
+}
+
+// Register slash commands
+registerCommands(process.env.APP_ID, process.env.DISCORD_BOT_TOKEN, commands);
