05d01dfd39792f6f83cfa14030f8ee96df9fb643
Pankaj <pankajshadev@gmail.com>
2025-07-05 23:40:39 +0530

    feat: add handler for delete-todo command chore: add await keyword before handler in base handler

--- src/dao/todos.ts
+++ src/dao/todos.ts
@@ -1,6 +1,7 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { CreateTodoDto } from "../dtos/todos";
 import { todos } from "../db/schema";
+import { eq } from "drizzle-orm";
 
 export const createTodo = async (discordId: string, todoDto: CreateTodoDto, env: Env) => {
     try{
@@ -19,6 +20,15 @@ export const createTodo = async (discordId: string, todoDto: CreateTodoDto, env:
         return res[0];
     }catch(error){
         console.error(`(createTodo): Error while creating todo`, error);
+        throw error;
+    }
+}
+
+export const deleteTodoById = async (id: number, env: Env) => {
+    try{
+        await drizzle(env.DB_URL).delete(todos).where(eq(todos.id, id));
+    }catch(error){
+        console.error(`(deleteTodoById): Error while deleting todo by id:${id}`, error);
         throw error;
     }
 }
--- src/handler/index.ts
+++ src/handler/index.ts
@@ -1,11 +1,12 @@
 import { InteractionResponseType, InteractionType } from "discord-interactions";
 import { IRequest } from "itty-router";
 import { JsonResponse } from "../dtos/response";
-import { ADD_TODO, HELLO, SIGNUP } from "../dtos/commands";
+import { ADD_TODO, DELETE_TODO, HELLO, SIGNUP } from "../dtos/commands";
 import { hello } from "./hello";
 import { signup } from "./signup";
 import { addTodo } from "./add-todo";
 import { Message, Priority } from "../dtos/todos";
+import { deleteTodo } from "./delete-todo";
 
 export const baseHandler = async (req: IRequest, env: Env, ctx: ExecutionContext) => {
     const message = await req.json() as Message;
@@ -24,13 +25,15 @@ export const baseHandler = async (req: IRequest, env: Env, ctx: ExecutionContext
             case HELLO.name.toLowerCase():
                 return hello(discordId);
             case SIGNUP.name.toLowerCase():
-               return signup(discordId, env);
+               return await signup(discordId, env);
             case ADD_TODO.name.toLowerCase():
-                return addTodo(discordId, {
+                return await addTodo(discordId, {
                     name: message.data.options[0].value,
                     priority: message.data.options[1].value as Priority,
                     description: message.data.options.length > 2 ? message.data.options[2].value : ""
                 }, env);
+            case DELETE_TODO.name.toLowerCase():
+                return await deleteTodo(discordId, message.data.options[0].value, env);
             default:
                 return new JsonResponse({ error: 'Unknown Type' }, { status: 400 });
         }
